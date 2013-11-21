5f088a24d154fb8420777ce0af1990d5793e78c5
goschtl <ck@novareto.de>
2013-11-21 11:37:01 +0100

    fixes

--- uvc/homefolder/components.py
+++ uvc/homefolder/components.py
@@ -8,34 +8,35 @@ from zope.traversing.browser.interfaces import IAbsoluteURL
 from zope.component import getUtility, adapter
 from zope.interface import implementer, provider
 from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.annotation.interfaces import IAttributeAnnotatable
 
 
-@implementer(IHomefolder)
+@implementer(IHomefolder, IAttributeAnnotatable)
 class Homefolder(BTreeContainer):
     pass
-    
+
 
 @implementer(IHomefolders)
 class Homefolders(BTreeContainer):
     default = Homefolder
     roles = [u'uvc.User', u'uvc.Editor', u'uvc.MasterUser']
-    
+
     def make_homefolder(self, factory=None):
-        klass= factory or self.default
+        klass = factory or self.default
         return klass()
 
     def write_homefolder(self, uid, homefolder):
         self[uid] = homefolder
-    
-    def grant_homefolder_access(self, uid, homefolder, roles=None):
+
+    def grant_homefolder_access(self, uid, homefolder, roles=[]):
         principal_roles = IPrincipalRoleManager(homefolder)
         for role in roles:
-            principal_roles.assignRoleToPrincipal(role, principalId)
+            principal_roles.assignRoleToPrincipal(role, uid)
 
     def assign_homefolder(self, uid):
-        homefolder = make_homefolder()
-        write_homefolder(uid, homefolder)
-        grant_homefolder_access(uid, homefolder, roles=self.roles)
+        homefolder = self.make_homefolder()
+        self.write_homefolder(uid, homefolder)
+        self.grant_homefolder_access(uid, homefolder, roles=self.roles)
 
     def get_homefolder(self, uid):
         return self.get(uid)
